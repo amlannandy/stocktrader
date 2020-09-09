3f1b69fc624252c5a7eb379dc40b23280927bd35
amlannandy <amlannandy5@gmail.com>
2020-09-09 20:48:52 +0530

    Fixes to Build

---
+++ src/firebaseCredentials.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: 'AIzaSyCeFdy56aTjdk84w8OjLVoUco-AYHi3BEU',
+  authDomain: 'stock-trader-4509b.firebaseapp.com',
+  databaseURL: 'https://stock-trader-4509b.firebaseio.com',
+  projectId: 'stock-trader-4509b',
+  storageBucket: 'stock-trader-4509b.appspot.com',
+  messagingSenderId: '703268817228',
+  appId: '1:703268817228:web:142a7a1329e51ddde467e9',
+  measurementId: 'G-MGJFNNVX8Q',
+};
